d548613adf8524c86a77f7ef8450a7fd56e115ff
Caroline Su <caroline77899@gmail.com>
2023-12-03 13:56:29 -0800

    add test file for move_character function

---
+++ test_move_character.py
@@ -0,0 +1,35 @@
+"""
+ADD A DOCSTRING
+"""
+from unittest import TestCase
+from character import move_character
+
+
+class Test(TestCase):
+    def test_move_north(self):
+        expect = {'X-coordinate': 2, 'Y-coordinate': 0, 'HP': 10, 'Max HP': 10, 'EX': 0, 'Level': 1, 'key': False}
+        character = {'X-coordinate': 2, 'Y-coordinate': 1, 'HP': 10, 'Max HP': 10, 'EX': 0, 'Level': 1, 'key': False}
+        direction = "N"
+        move_character(character, direction)
+        self.assertEqual(expect, character)
+
+    def test_move_south(self):
+        expect = {'X-coordinate': 3, 'Y-coordinate': 4, 'HP': 7, 'Max HP': 10, 'EX': 3, 'Level': 1, 'key': False}
+        character = {'X-coordinate': 3, 'Y-coordinate': 3, 'HP': 7, 'Max HP': 10, 'EX': 3, 'Level': 1, 'key': False}
+        direction = "S"
+        move_character(character, direction)
+        self.assertEqual(expect, character)
+
+    def test_move_west(self):
+        expect = {'X-coordinate': 0, 'Y-coordinate': 3, 'HP': 5, 'Max HP': 10, 'EX': 0, 'Level': 1, 'key': False}
+        character = {'X-coordinate': 1, 'Y-coordinate': 3, 'HP': 5, 'Max HP': 10, 'EX': 0, 'Level': 1, 'key': False}
+        direction = "W"
+        move_character(character, direction)
+        self.assertEqual(expect, character)
+
+    def test_move_east(self):
+        expect = {'X-coordinate': 4, 'Y-coordinate': 1, 'HP': 10, 'Max HP': 10, 'EX': 0, 'Level': 1, 'key': True}
+        character = {'X-coordinate': 3, 'Y-coordinate': 1, 'HP': 10, 'Max HP': 10, 'EX': 0, 'Level': 1, 'key': True}
+        direction = "E"
+        move_character(character, direction)
+        self.assertEqual(expect, character)
